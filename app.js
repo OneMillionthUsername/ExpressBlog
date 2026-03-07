413039b1a857920b574068c8813d02fec8160830
OneMillionthUsername <dean.marinov@gmx.at>
2026-03-07 17:34:34 +0100

    fix(helmet): correct styleSrcAttr syntax for inline styles

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ app.use(helmet({
         'https://cdn.tiny.cloud',
         // Nonce wird dynamisch hinzugefügt für Inline-Styles (nonceMiddleware)
       ],
-      styleSrcAttr: ["'unsafe-inline'"],
+      styleSrcAttr: ['\'unsafe-inline\''],
       fontSrc: [
         '\'self\'',
         'https://fonts.gstatic.com',
